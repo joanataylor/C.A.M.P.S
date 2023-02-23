37a5ab042eac4ab30fe01dc8816907873a3c17ca
Joana Grave <joana_grave@hotmail.com>
2023-02-23 18:28:34 -0500

    deleted imports and imported navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,16 @@
 import './App.css';
-import Appbar from './components/Appbar';
-import Student from './components/Student';
-import Navbar from './components/Navbar';
 import {Routes ,Route} from "react-router-dom"
 import Welcome from './pages/Welcome';
 import DashBoard from './pages/DashBoard';
 import CounselorDetail from './pages/CounselorDetail';
 import ActivityDetail from './pages/ActivityDetail';
 import Login from './pages/Login';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />     
       <Routes>
         <Route path="/" element={<Welcome/>}/>
         <Route path="/login" element={<Login />}/>
