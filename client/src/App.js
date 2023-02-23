a5ebdbc4be4a2d1f3557f71b839e293ea93d37b9
Brandon DeBenedictis <brandonchasedebenedictis@gmail.com>
2023-02-23 15:15:28 -0800

    added-pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,23 @@ import Appbar from './components/Appbar';
 import Student from './components/Student';
 import Navbar from './components/Navbar';
 import {Routes ,Route} from "react-router-dom"
+import Welcome from './pages/Welcome';
+import DashBoard from './pages/DashBoard';
+import CounselorDetail from './pages/CounselorDetail';
+import ActivityDetail from './pages/ActivityDetail';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Navbar />}/>
-        <Route path="/login" element={<Navbar />}/>
-        <Route path="/dashboard" element={<Navbar />}/>
+        <Route path="/" element={<Welcome/>}/>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/dashboard" element={<DashBoard />}/>
+        <Route path="/counselor" element={<CounselorDetail />}/>
+        <Route path="/activity" element={<ActivityDetail />}/>
 
       </Routes>
+
     </div>
   );
 }
