1f587d8a8702295cb144efdaf8119c4f5988d33f
Joana Grave <joana_grave@hotmail.com>
2023-02-24 18:52:35 -0500

    added main card

--- client/src/pages/CounselorDetail.jsx
+++ client/src/pages/CounselorDetail.jsx
@@ -1,232 +1,130 @@
 import { Container } from "@mui/system";
-import React from "react";
-import counselorJo from "../images/jojo.jpeg";
-import counselorEthan from "../images/ethan.jpeg";
-import counselorGokhan from "../images/gokhan.jpeg";
-import counselorBrandon from "../images/brandon.jpeg";
-import counselorHasan from "../images/hasan.jpeg";
-import counselorVincent from "../images/vincent.jpeg";
+import React, { useState } from "react";
 import { Button } from "@mui/material";
 import "./styles/counselor.css";
 
 function CounselorDetail() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageChange = (imageId) => {
+    const selectedImage = images.find((image) => image.id === imageId);
+    setSelectedImage(selectedImage);
+  };
+  const counselors = [
+    { name: "Ethan", Location: "Arizona" },
+    { name: "Gokhan", Location: "Turkey" },
+    { name: "Brandon", Location: "California" },
+    { name: "Joana", Location: "Portugal" },
+    { name: "Ethan", Location: "Arizona" },
+    { name: "Gokhan", Location: "Turkey" },
+    { name: "Ethan", Location: "Arizona" },
+    { name: "Brandon", Location: "California" },
+    { name: "Joana", Location: "Portugal" },
+    
+  ];
+
+  const images = [
+    {
+      id: 1,
+      src: "https://media.licdn.com/dms/image/C4E03AQFOGCpxF3oHVQ/profile-displayphoto-shrink_800_800/0/1656388466701?e=1682553600&v=beta&t=gEWxp-U8xykoEqJZO3JNYvI5wj_G34K4g194pa7zAb8",
+      alt: "Gokhan Sigircik",
+    },
+    {
+      id: 2,
+      src: "https://media.licdn.com/dms/image/C5603AQFWzIHj7Ijo2g/profile-displayphoto-shrink_800_800/0/1653944302306?e=1682553600&v=beta&t=4SBjWI3gH5OfT7Sm4-Xe1sWixose0YD9SuFjRZthEgg",
+      alt: "Ethan Smith",
+    },
+    {
+      id: 3,
+      src: "https://media.licdn.com/dms/image/D5635AQEJSvwkYb6jrw/profile-framedphoto-shrink_800_800/0/1676223550707?e=1677873600&v=beta&t=TdWJ-46nftzDWfJHeehT9DmXF3WLXgjYhSjWoh0aVAE",
+      alt: "Brandon DeBenedectis",
+    },
+    {
+      id: 4,
+      src: "https://media.licdn.com/dms/image/D5603AQGSRUXV7UZdGg/profile-displayphoto-shrink_400_400/0/1668811669457?e=1682553600&v=beta&t=yY14ue6KYvAKAeHso6C1inNWJHslP7ycuTsMSYPlGdU",
+      alt: "Joana Grave",
+    },
+    {
+      id: 5,
+      src: "https://media.licdn.com/dms/image/C4E03AQFOGCpxF3oHVQ/profile-displayphoto-shrink_800_800/0/1656388466701?e=1682553600&v=beta&t=gEWxp-U8xykoEqJZO3JNYvI5wj_G34K4g194pa7zAb8",
+      alt: "Gokhan Sigircik",
+    },
+    {
+      id: 6,
+      src: "https://media.licdn.com/dms/image/C5603AQFWzIHj7Ijo2g/profile-displayphoto-shrink_800_800/0/1653944302306?e=1682553600&v=beta&t=4SBjWI3gH5OfT7Sm4-Xe1sWixose0YD9SuFjRZthEgg",
+      alt: "Ethan Smith",
+    },
+    {
+      id: 7,
+      src: "https://media.licdn.com/dms/image/D5635AQEJSvwkYb6jrw/profile-framedphoto-shrink_800_800/0/1676223550707?e=1677873600&v=beta&t=TdWJ-46nftzDWfJHeehT9DmXF3WLXgjYhSjWoh0aVAE",
+      alt: "Brandon DeBenedectis",
+    },
+    {
+      id: 8,
+      src: "https://media.licdn.com/dms/image/D5603AQGSRUXV7UZdGg/profile-displayphoto-shrink_400_400/0/1668811669457?e=1682553600&v=beta&t=yY14ue6KYvAKAeHso6C1inNWJHslP7ycuTsMSYPlGdU",
+      alt: "Joana Grave",
+    },
+    {
+      id: 9,
+      src: "https://media.licdn.com/dms/image/C4E03AQFOGCpxF3oHVQ/profile-displayphoto-shrink_800_800/0/1656388466701?e=1682553600&v=beta&t=gEWxp-U8xykoEqJZO3JNYvI5wj_G34K4g194pa7zAb8",
+      alt: "Joana Grave",
+    },
+  ];
+
   return (
     <Container>
       <h1>Counselors</h1>
-      {/* <Paper elevation={24}> */}
-        <div className="d-flex flex-row justify-content-center align-items-center mb-3">
-          <div className="card my-card w-25 shadow mr-3">
-            <img
-              // className="img-fluid"
-              className="card-img-top img-fluid"
-              src={counselorJo}
-              // style={{ height: "240px" }}
-              class="img-thumbnail"
-              alt="img-thumbnail"
-            />
+      <div className="d-flex">
+        <div className="d-flex">
+          <div className="card my-main-card">
+            <div className="card-header">
+              <h1>Ethan Smith</h1>
+            </div>
+            {selectedImage && (
+              <img
+                className="card-img-top img-fluid"
+                src={selectedImage.src}
+                alt="Ethans pic"
+              />
+            )}
             <div className="card-body">
-              <h5 className="card-title">Joana Grave</h5>
               <p className="card-text">
-                Portugal
+                There are many variations of passages of Lorem Ipsum available,
+                but the majority have suffered alteration in some form, by
+                injected humour, or randomised words which don't look even
+                slightly believable. If you are going to use a passage of Lorem
+                Ipsum, you need to be sure there isn't anything embarrassing
+                hidden in the middle of text. All the Lorem Ipsum generators on
               </p>
             </div>
-            <div className="card-overlay">
-              <h5 className="card-title">Fun Facts:</h5>
-              <p className="card-text">
-                I speak 3 languages, I have 3 sisters and I want to have 3 more babies.
-              </p>
-            </div>
-          </div>
-
-          <div className="card shadow my-card w-25 mr-3">
-            <img
-              // className="img-fluid"
-              className="card-img-top img-fluid"
-              src={counselorEthan}
-              // style={{ height: "240px" }}
-              class="img-thumbnail"
-              alt="img-thumbnail"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Ethan Smith</h5>
-              <p className="card-text">
-                Arizona
-              </p>
-            </div>
-            <div className="card-overlay">
-              <h5 className="card-title">Fun Facts:</h5>
-              <p className="card-text">
-                I've been a nurse, a scientist and a web developer, and now a camp counselor.
-              </p>
-            </div>
-          </div>
-
-          <div className="card shadow my-card w-25">
-            <img
-              // className="img-fluid"
-              className="card-img-top img-fluid"
-              src={counselorGokhan}
-              // style={{ height: "240px" }}
-              class="img-thumbnail"
-              alt="img-thumbnail"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Gokhan Sigircik</h5>
-              <p className="card-text">
-                Turkey
-              </p>
-            </div>
-            <div className="card-overlay">
-              <h5 className="card-title">Fun Facts:</h5>
-              <p className="card-text">
-                I speak 4 languages and ive travelled to 35 countries.
-              </p>
-            </div>
+            <Button variant="contained">Message Counselor</Button>
           </div>
         </div>
-      {/* </Paper> */}
 
-      {/* <Paper elevation={24}> */}
-        <div className="d-flex flex-row justify-content-center align-items-center mb-3">
-
-          <div className="card shadow my-card w-25 mr-3">
-            <img
-              // className="img-fluid"
-              className="card-img-top img-fluid"
-              src={counselorBrandon}
-              // style={{ height: "240px" }}
-              class="img-thumbnail"
-              alt="img-thumbnail"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Brandon DeBenedectis</h5>
-              <p className="card-text">
-                California
-              </p>
+        <div className="d-flex row justify-content-end align-items-center">
+          {counselors.map((counselor, index) => (
+            <div className="card shadow my-card w-25 mr-3" key={index}>
+              <img
+                className="card-img-top img-fluid"
+                key={images[index].id}
+                src={images[index].src}
+                alt={images[index].alt}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{counselor.name}</h5>
+                <p className="card-text">{counselor.Location}</p>
+              </div>
+              <div
+                className="card-overlay"
+                onClick={() => handleImageChange(images[index].id)}
+              >
+                <h5 className="card-title">Fun Facts</h5>
+                <p className="card-text">Brief description goes here</p>
+              </div>
             </div>
-            <div className="card-overlay">
-              <h5 className="card-title">Fun Facts</h5>
-              <p className="card-text">
-                I live in a snow globe and i'm a triplet.
-              </p>
-            </div>
-          </div>
-
-          <div className="card shadow my-card w-25 mr-3">
-            <img
-              // className="img-fluid"
-              className="card-img-top img-fluid"
-              src={counselorHasan}
-              // style={{ height: "240px" }}
-              class="img-thumbnail"
-              alt="img-thumbnail"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Hasan Huda</h5>
-              <p className="card-text">
-                New York
-              </p>
-            </div>
-            <div className="card-overlay">
-              <h5 className="card-title">Fun Facts:</h5>
-              <p className="card-text">
-                Likes to sleep in and do algos.
-              </p>
-            </div>
-          </div>
-
-          <div className="card shadow my-card w-25">
-            <img
-              // className="img-fluid"
-              className="card-img-top img-fluid"
-              src={counselorVincent}
-              // style={{ height: "240px" }}
-              class="img-thumbnail"
-              alt="img-thumbnail"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Vincent Guerena</h5>
-              <p className="card-text">
-                California
-              </p>
-            </div>
-            <div className="card-overlay">
-              <h5 className="card-title">Fun Facts</h5>
-              <p className="card-text">
-                Cool mustache and chill.
-              </p>
-            </div>
-          </div>
-
+          ))}
         </div>
-
-        <Button variant="contained">Message Counselor</Button>
-      {/* </Paper> */}
-
-      {/* <div className="d-flex flex-row justify-content-center align-items-center p-5">
-        <Paper elevation={24}>
-          <Card sx={{ maxWidth: 345 }} className="p-3 mb-2">
-            <CardMedia
-              sx={{ height: 270 }}
-              image={counselorEthan}
-              style={{
-                width: "200px",
-                height: "200px",
-                // borderRadius: "50%",
-                margin: "auto",
-              }}
-              title="counselorOne"
-            />
-          </Card>
-
-          <Card sx={{ maxWidth: 345 }} className="p-3 mb-2">
-            <CardMedia
-              sx={{ height: 270 }}
-              image={counselorJo}
-              style={{
-                width: "200px",
-                height: "200px",
-                // borderRadius: "50%",
-                margin: "auto",
-              }}
-              title="counselorOne"
-            />
-          </Card>
-
-          <Card sx={{ maxWidth: 345 }} className="p-3">
-            <CardMedia
-              sx={{ height: 270 }}
-              image={counselorGokhan}
-              style={{
-                width: "200px",
-                height: "200px",
-                // borderRadius: "50%",
-                margin: "auto",
-              }}
-              title="counselorOne"
-            />
-          </Card>
-        </Paper>
       </div>
-      <Card sx={{ maxWidth: 345 }}>
-        <CardMedia
-          sx={{ height: 270 }}
-          image={counselorJo}
-          title="counselorOne"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Joana Grave
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Born and raised in Portugal.
-          </Typography>
-        </CardContent>
-        {/* <CardActions>
-                  <Button size="small">Share</Button>
-                  <Button size="small">Learn More</Button>
-              </CardActions> */}
-      {/* </Card> */}
     </Container>
   );
 }
